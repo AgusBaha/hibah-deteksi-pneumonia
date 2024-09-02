e9d5fd8353eba300540c2fd6c374c3fac7b818f1
idkodekreatif <id.kodekreatif@gmail.com>
2024-09-02 19:40:27 +0700

    "Added columns and foreign key constraints to questions, answers, and results tables"

--- database/migrations/2024_09_02_123655_create_answers_table.php
+++ database/migrations/2024_09_02_123655_create_answers_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('question_id');
+            $table->boolean('answer');
             $table->timestamps();
+
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
         });
     }
 
